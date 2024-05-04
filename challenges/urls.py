c90ba258ad1ed3d01322be9b74f4049f9e327136
Varun <bhat.varun.25@gmail.com>
2024-05-04 09:31:57 +0530

    added name argument

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<month>", views.monthly_challenge),
+    path("<month>", views.monthly_challenge, name = "monthly_challenge"),
 ]
